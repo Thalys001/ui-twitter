b9e7e45777acca8bb3f9d82d66e6be14bba2d556
Thalys001 <thalyssouza708@gmail.com>
2023-02-19 13:39:31 -0300

    feat(timeline/status): capturing form data

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,36 +1,48 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 
-
-
-const tweets = [
-  'meu primeiro tweet',
-  'teste',
-  'deu certo tweetar'
-]
-
 import './Timeline.css'
 
 export function Timeline() {
+  const [newTweet, setNewTweet] = useState('')
+  const [tweets, setTweets] = useState([
+    'meu primeiro tweet',
+    'teste',
+    'deu certo tweetar'
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+
+    setTweets([newTweet, ...tweets])
+    setNewTweet('')
+  }
+
   return (
     <main className="timeline">
-    <Header title='Home' />
+      <Header title='Home' />
 
+      <form onSubmit={createNewTweet} className="new-tweet-form">
+        <label htmlFor="tweet">
+          <img src="https://github.com/thalys001.png" alt="Thalys Leite" />
+          <textarea id="tweet"
+            placeholder="What's happenning?"
+            value={newTweet}
+            onChange={(event) => {
+              setNewTweet(event.target.value)
+            }}
+          />
+        </label>
 
-    <form className="new-tweet-form">
-      <label htmlFor="tweet">
-        <img src="https://github.com/thalys001.png" alt="Thalys Leite" />
-        <textarea id="tweet" placeholder="What's happenning?" />
-      </label>
+        <button type='submit'>Tweet</button>
+      </form>
+      <Separator />
 
-      <button type='submit'>Tweet</button>
-    </form>
-    <Separator />
-
-    {tweets.map(tweet => {
-      return <Tweet key={tweet} content={tweet} />
-    })}
-  </main>
+      {tweets.map(tweet => {
+        return <Tweet key={tweet} content={tweet} />
+      })}
+    </main>
   )
 }
