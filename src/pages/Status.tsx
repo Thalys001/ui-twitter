14bb735be0069c85338a2945673bff3adafaca40
Thalys001 <thalyssouza708@gmail.com>
2023-02-20 12:33:14 -0300

    feat(timeline/status): adding onkeydown function

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,9 +1,19 @@
-import { FormEvent, useState } from "react"
+import { FormEvent, KeyboardEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 
 import './Status.css'
+
+// Fluxo de renderização: 
+// 1. Toda vez que alteramos o estado de um componente, TODO componente é recalculado.
+// 2. Toda vez que o seu componente PAI renderizar
+// 3. Toda vez que alguma das suas propriedades mudarem.
+
+// Algoritmo de reconciliação:
+// 1. Criar em memória a nova versão do HTML do componente
+// 2. Compara essa nova versão com a versão anterior do HTML (Diff)
+// 3. Aplicar as operações JavaScript para alterar somente o necessário no HTML 
 
 export function Status() {
   const [newAnswer, setNewAnswer] = useState('')
@@ -19,6 +29,14 @@ export function Status() {
     setAnswers([newAnswer, ...answers])
     setNewAnswer('')
   }
+
+  function handleHotKeySubmit(event: KeyboardEvent) {
+    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+      setAnswers([newAnswer, ...answers])
+      setNewAnswer('')
+    }
+  }
+
   return (
     <main className="status">
       <Header title='Tweet' />
@@ -26,15 +44,16 @@ export function Status() {
 "/>
       <Separator />
 
-      <form  onSubmit={createNewAnswer} className="answer-tweet-form">
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/thalys001.png" alt="Thalys Leite" />
-          <textarea id="tweet" 
-          placeholder="Tweet your answer" 
-          value={newAnswer}
-          onChange={(event) => {
+          <textarea id="tweet"
+            placeholder="Tweet your answer"
+            value={newAnswer}
+            onKeyDown={handleHotKeySubmit}
+            onChange={(event) => {
               setNewAnswer(event.target.value)
-          }}
+            }}
           />
         </label>
 
